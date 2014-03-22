9e66454f575387845cbd0f0f5558f5c6117780f9
everfor <alan.yang1994@gmail.com>
2014-03-22 11:46:34 -0400

    Added angular js for frontend frames

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
 <html><body><h1>It works!</h1>
-<?php
-try {
-	$host = "localhost";
-	$db = "wordpress";
-    $dbh = new PDO("mysql:host={$host};dbname={$db}", "root", "yg19940308");
-    foreach($dbh->query('SELECT * from wp_links') as $row) {
-        echo $row["link_id"] . " " . $row["link_url"];
-        echo "<br/>";
-    }
-    $dbh = null;
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
-?>
+
+<form action="api/example">
+    <input type="text"></input>
+    <button type="submit"></button>
+</form>
 </body></html>
